6a5e5fb9e74a68decfe9c14e54a4d7a78e3a7767
Nur Azizah <nur.azizah.7241@gmail.com>
2022-12-22 23:47:34 +0700

    fixing

--- DOrphan_Mobile/app/src/main/java/com/example/dorphan/Helpers/Const.java
+++ DOrphan_Mobile/app/src/main/java/com/example/dorphan/Helpers/Const.java
@@ -2,6 +2,4 @@ package com.example.dorphan.Helpers;
 
 public class Const {
     public static final String BASE_URL = "http://se.dorphan.archotech.site/api/";
-    //public static final String BASE_URL = "http://dorphan.archotech.site/api/";
-    //public static final String BASE_URL = "http://azizah.archotech.site/nurazizah24/Phys8/public/api/";
 }
